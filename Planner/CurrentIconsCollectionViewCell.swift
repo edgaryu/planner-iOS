7e8a8f56d52d0a66013afcfb94b8a93e3e0ce6ad
Edgar <mredgaryu@gmail.com>
2018-03-11 18:49:12 -0700

    add subroutine

---
+++ Planner/CurrentIconsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CurrentIconsCollectionViewCell.swift
+//  Planner
+//
+//  Created by Edgar Yu on 3/11/18.
+//  Copyright © 2018 AppleInc. All rights reserved.
+//
+
+import UIKit
+
+class CurrentIconsCollectionViewCell: UICollectionViewCell {
+    
+}
